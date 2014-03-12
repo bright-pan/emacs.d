78be1a97b036c72bfadd465d1a26629aa5747d4a
Bright Pan <loststriker@gmail.com>
2014-03-12 15:25:13 +0800

    Add gtags update function

--- lisp/init-cc-mode.el
+++ lisp/init-cc-mode.el
@@ -7,6 +7,7 @@
 (add-to-list 'ac-modes 'cc-mode)
 
 (defun my-c-mode-hook ()
+  (ggtags-mode t) ;; enable gtags in cc-mode
   (setq c-basic-offset 4          ;; 基本缩进宽度
 	indent-tabs-mode t        ;; 禁止空格替换Tab
 	default-tab-width 4)      ;; 默认Tab宽度
